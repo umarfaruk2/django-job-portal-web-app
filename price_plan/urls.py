eb4ce6d039091df941c1f2441c0f0f70e77aee5a
umarfaruk2 <farukdev22@gmail.com>
2023-09-28 11:50:15 +0600

    update deploy setting

--- price_plan/urls.py
+++ price_plan/urls.py
@@ -1,5 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('price_plan/', views.price_plan, name='price_plan'),
@@ -7,4 +10,6 @@ urlpatterns = [
     path('success/', views.success_view, name='success'),
     path('faild/', views.faild_view, name='faild'),
     path('free_plan/', views.free_plan, name='free_plan'),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
